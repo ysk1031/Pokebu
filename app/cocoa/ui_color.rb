857c2e0944b1db75559d7dca0eba25e1490ded2e
Yusuke Aono <yusukeaono.1031@gmail.com>
2015-01-25 19:10:11 +0900

    Create UIColor class-method to call theme color

---
+++ app/cocoa/ui_color.rb
@@ -0,0 +1,11 @@
+class UIColor  
+  def self.themeColorGreen
+    # 50BCB6
+    self.colorWithRed(0.314, green: 0.737, blue: 0.714, alpha: 1.0)
+  end
+
+  def self.themeColorRed
+    # ED4055
+    self.colorWithRed(0.929, green: 0.251, blue: 0.333, alpha: 1.0)
+  end
+end
